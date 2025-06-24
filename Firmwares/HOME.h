c4bfa20ab39ac7b96d0beda85760df434ea12e43
MUR4881 <usmanmurabi@gmail.com>
2025-06-24 14:43:43 +0100

    Create Header File

---
+++ Firmwares/HOME.h
@@ -0,0 +1,14 @@
+#ifndef HOME_H
+#define HOME_H
+
+/** Include Dependencies **/
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <AsyncTCP>
+
+extern "C" {
+
+	#include "freertos/FreeRTOS.h"
+	#include "freertos/times.h"
+
+}
