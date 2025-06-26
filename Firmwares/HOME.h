6aa366704aae6fd7abc3ac977704bf3e4614b510
MUR4881 <usmanmurabi@gmail.com>
2025-06-26 16:02:02 +0100

    Add more function declarations & Define variables:
    struct_message: for messages over espnow
    mac_address_t: for mac addresses types
    struct_pairing: for pairing over espnow
    MessageType: for knowing the type of message sent
    
    there is more unimplemented for commands and events!

--- Firmwares/HOME.h
+++ Firmwares/HOME.h
@@ -2,9 +2,12 @@
 #define HOME_H
 
 /** Include Dependencies **/
+#include <esp_now.h>
+#include <esp_wifi.h>
 #include <WiFi.h>
 #include <ESPAsyncWebServer.h>
-#include <AsyncTCP>
+#include <AsyncTCP.h>
+#include <ArduinoJson.h>
 
 extern "C" {
 
@@ -13,10 +16,70 @@ extern "C" {
 
 }
 
+/**			Macros and Unions and types					**/
+
+/**
+ * enum MessageType -  an enum for checking for the type sent or received
+ * @PAIRING: to show that the packet, just sent is for pairing
+ * @COMMAND: to show that the packet sent is a command
+ * @DATA: to show that the packet sent is a sensor DATA
+ * @EVENT: to show that the packet sent is an Event
+ */
+
+enum MessageType
+{
+	PAIRING,
+	COMMAND,
+	DATA,
+	EVENT
+};
+
+typedef uint8_t [6] mac_address_t;
+
+/* Currently thinking about changing, id to whatever, the master assigns*/
+/* to the slave */
+
+/**
+ * Struct struct_pairing - to Contain Pairing messages
+ * @msgType: either PAIRING, COMMAND or DATA
+ * @id: the id assigned to the given board on the network
+ * @macAddr: The Mac Address of the pairing Device
+ * @channel: The WiFi Channel of communication
+ */
+
+typedef struct struct_pairing
+	{
+		uint8_t msgType;
+		uint8_t id;
+		uint8_t macAddr[6];
+		uint8_t channel;
+	} pairing_t;	/* type for pairing message */
+
+/**
+ * Struct struct_message - acting a mould for packages sent over espnow
+ * @msgType: either PAIRING, COMMAND or DATA
+ * @id: the board id on the network
+ * @volt: the voltage reading from the sensor
+ * @current: The current reading from the sensor
+ * @power: The power voltage * current
+ * @readingId: The packet number for the sent packet
+ */
+
+typedef struct_message {
+	uint8_t msgType;
+	uint8_t id;
+	uint8_t volt;
+	uint8_t current;
+	uint8_t power;
+	uint8_t readingId;
+}
+
+
 /** helper functions **/
 
 void WiFiEvent(WiFiEvent_t event);
 
 /** Preprocessing functions **/
-String WiFiScan();
+String WiFiScan(void);
 
+/** ESP NOW functionalities */
